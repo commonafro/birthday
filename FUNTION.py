c3da876b875fbc79e135eff40f12fb38a23fece3
Evan Black <clutchman367@gmail.com>
2022-01-25 12:46:16 -0800

    Created an intake program that asks user for their birthday and social security #

--- FUNTION.py
+++ FUNTION.py
@@ -1,1 +1,9 @@
+import datetime
+
 # YOU DONT EXIST YOU DONT EXIST YOU DONT EXIST YOU DONT EXIST
+m = input("What is the month of your birthday: ")
+y = input("What year were you born: ")
+d = input("What day were you born: ")
+ss = input("What's your social security number: ")
+
+print("You were born on: ", m, d, y, "Your social security is ", ss) 
